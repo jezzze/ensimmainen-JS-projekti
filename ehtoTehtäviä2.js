ee6154704324d607eaea5cfd442448e3daeaec49
unknown <jesse.mitronen@edu.keuda.fi>
2020-08-31 11:26:38 +0300

    ehtoTehtäviä2.js

---
+++ ehtoTehtäviä2.js
@@ -0,0 +1,36 @@
+function viikonpaiva()
+{
+  var luku = document.getElementById('luku').value;
+ if(luku == 1)
+}
+
+{
+tulosta2').innerHTML = "Maanantai";
+}
+{
+  document.getElementById('tulosta2').innerHTML = "Tiistai";
+  }
+  else if(vluku == 3)
+  {
+    document.getElementById('tulosta2').innerHTML = "Keskiviikko";
+  }
+  else if(vluku == 4)
+  {
+    document.getElementById('tulosta2').innerHTML = "Torstai";
+  }
+  else if(vluku == 5)
+  {
+    document.getElementById('tulosta2').innerHTML = "Perjantai";
+  }
+  else if(vluku == 6)
+  {
+    document.getElementById('tulosta2').innerHTML = "Lauantai";
+  }
+  else if(vluku == 7)
+  {
+    document.getElementById('tulosta2').innerHTML = "Sunnuntai";
+ }
+ else {
+     document.getElementById('tulosta2').innerHTML = "Luku ei kuulu viikonpäivälle";
+ }
+}
