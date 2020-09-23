6174c738160d51834bd226ff97642ff63ae4e8e3
unknown <jesse.mitronen@edu.keuda.fi>
2020-09-23 11:04:36 +0300

    ad

---
+++ keskiviikon 19.8.2020 tehtavat/tehtava5.js
@@ -0,0 +1,15 @@
+function kaannakieli()
+{
+  var lang = document.getElementById('kielet').value;
+     if(lang == 'eng')
+{
+  document.write("Hello world!");
+}
+else if (lang == 'ger')
+{
+document.write("Hallo Welt!");
+}
+else {
+  document.write("Hei Verden!")
+}
+}
