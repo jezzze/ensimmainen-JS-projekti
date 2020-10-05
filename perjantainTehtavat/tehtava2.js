6283a9db88477da1efbe1aadd3b79532d191d990
jezzze <jesse.mitronen@gmail.com>
2020-10-05 13:50:01 +0300

    asd

---
+++ perjantainTehtavat/tehtava2.js
@@ -0,0 +1,31 @@
+function Lotto()
+{
+  var lnumerot = [];
+  var temp;
+  for(var lotto = 0; lotto < 7; lotto++)
+  {
+    temp = Math.floor(Math.random()*40)+1;
+    lnumerot[lotto] = temp;
+  }
+  lnumerot.sort(/*function(a, b){return a-b}*/);
+
+  document.getElementById('taulu').innerHTML = '<p>'+lnumerot+'</p>';
+}
+
+function taulukko()
+{
+  var jono = [];
+  jono = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+  var taulu = '<table border="1">';
+  for(var j = 0; j < jono.length; j++)
+  {
+    taulu += '<tr>';
+    for(var k = 0; k < jono[j].length; k++)
+    {
+      taulu += '<td>' + jono[j][k] + '</td>';
+    }
+    taulu += '</tr>';
+  }
+  taulu += '</table>';
+  document.getElementById('taulu2').innerHTML = taulu;
+}
