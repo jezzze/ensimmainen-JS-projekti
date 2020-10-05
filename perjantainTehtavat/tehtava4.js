9323e59be2799cfddc2a5f30d5d0f2f702015b6b
jezzze <jesse.mitronen@gmail.com>
2020-10-05 13:57:43 +0300

    okok

---
+++ perjantainTehtavat/tehtava4.js
@@ -0,0 +1,30 @@
+function Kortit()
+{
+  var pakka = [''];
+  var maat = ['&#9828;', '&#9827;', '&#9826;', '&#9825;'];
+  //var maat = ['pa','ri','ru', 'he']
+  var numerot = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+  laskuri = 0;
+  for(m = 0; m < maat.length; m++)
+  {
+    for(n = 0; n < numerot.length; n++)
+    {
+      if(m == maat.length-1 && n == numerot.length-1)
+      {
+        pakka[laskuri] = maat[m] + numerot[n];
+      }
+      else {
+        pakka[laskuri] = maat[m]+numerot[n];
+        laskuri++;
+      }
+
+    }
+  }
+  var kortit = [];
+  for(l = 0; l < 5; l++)
+  {
+      temp = pakka[Math.floor(Math.random()*52)];
+      kortit[l] = temp;
+  }
+  document.getElementById('korttipakka').innerHTML = '<p>'+kortit+'</p>';
+}
