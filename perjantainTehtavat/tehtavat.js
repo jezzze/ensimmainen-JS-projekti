ba2cfad556f7e2691ee32fe9f2f8bae18d8325ee
unknown <jesse.mitronen@edu.keuda.fi>
2020-10-09 08:58:00 +0300

    hfd

--- perjantainTehtavat/tehtavat.js
+++ perjantainTehtavat/tehtavat.js
@@ -5,36 +5,46 @@ function Pisteet()
   for(var i = 0; i < annettuSana.length; i++)
   {
     switch (annettuSana[i]) {
-      case 'a':
-      case 'n':
-      case 't':
       case 'A':
+      case 'E':
+      case 'I':
       case 'N':
       case 'S':
       case 'T':
         pisteet++;
         break;
+      case 'O':
+      case 'Ä':
+      case 'K':
+      case 'L':
+        pisteet+=2;
         break;
-      case 'V':
-      case 'i':
-      case 'l':
-        pisteet+=-10;
+      case 'U':
+      case 'M':
+        pisteet+=3;
         break;
-      case 'b':
-      case 'f':
-      case 'u':
-      case 'B':
-      case 'm':
-      case 'n':
-        pisteet+=8;
-        break;
+      case 'Y':
+      case 'H':
       case 'J':
-      case 'e':
-      case 's':
-        pisteet+=100;
-        break;
+      case 'P':
+      case 'R':
+      case 'V':
+      pisteet+=4;
+      break;
+      case 'Ö':
+      case 'D':
+      pisteet+=7;
+      break;
+      case 'B':
+      case 'F':
+      case 'G':
+      pisteet+=8;
+      break;
+      case 'C':
+      pisteet+=10;
+      break;
       default:
-        pisteet+=12;
+      pisteet+=12;
     }
     document.getElementById('scrabble').innerHTML = "<p>Sanan " + annettuSana + " pisteet ovat: " + pisteet + "</p>";
   }
